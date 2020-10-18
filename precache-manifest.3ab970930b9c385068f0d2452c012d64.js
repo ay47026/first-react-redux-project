13d66c94727cc7788220a6ff7648a366d0c4180f
ay47026 <ay47026@gmail.com>
2020-10-18 12:27:27 +0530

    Updates

---
+++ precache-manifest.3ab970930b9c385068f0d2452c012d64.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8396b8d57561b982f355541e648e370",
+    "url": "/first-react-redux-project/index.html"
+  },
+  {
+    "revision": "d830eb9c76aabe39c8d6",
+    "url": "/first-react-redux-project/static/css/2.355b5af7.chunk.css"
+  },
+  {
+    "revision": "56166c2f57b4cf6c76e3",
+    "url": "/first-react-redux-project/static/css/main.a9f091e2.chunk.css"
+  },
+  {
+    "revision": "d830eb9c76aabe39c8d6",
+    "url": "/first-react-redux-project/static/js/2.6609fb0e.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/first-react-redux-project/static/js/2.6609fb0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56166c2f57b4cf6c76e3",
+    "url": "/first-react-redux-project/static/js/main.dae50274.chunk.js"
+  },
+  {
+    "revision": "63fcfd0ca39904b6376b",
+    "url": "/first-react-redux-project/static/js/runtime-main.e6e7b1ba.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/first-react-redux-project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/first-react-redux-project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/first-react-redux-project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/first-react-redux-project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/first-react-redux-project/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
